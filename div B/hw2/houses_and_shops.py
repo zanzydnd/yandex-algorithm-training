45db3f8e7b57b9911d91d4e1581aede3994b4e54
zanzydnd <zanzydnd@gmail.com>
2021-09-02 14:45:09 +0300

    shops_and_houses

--- div B/hw2/houses_and_shops.py
+++ div B/hw2/houses_and_shops.py
@@ -2,8 +2,23 @@ buildings = list(map(int, input().split()))
 
 # 1 - жилой
 # 2 - магазин
-shops = set()
+local_min = 0
+maximum = 0
 for i in range(len(buildings)):
-    if buildings[i] == 2:
-        shops.add(i)
+    left = 10
+    right = 10
+    if buildings[i] == 1:
+        for m in reversed(range(0, i)):
+            if buildings[m] == 2:
+                left = i - m
+                break
+        for l in range(i, 10):
+            if buildings[l] == 2:
+                right = l - i
+                break
+        local_min = min(right, left)
+        # print("right: ", right, "left:", left)
+        # print("house n: ", i, "shortest distance: ", local_min)
+    maximum = max(local_min, maximum)
 
+print(maximum)
